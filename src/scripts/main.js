3306a175377a341dc3b6f4ef2e9970064ba21a7a
Anastasiia Boreiko <kravchenkoanastasiia92@gmail.com>
2025-04-30 12:24:57 +0300

    commit message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,27 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+  const ulElem = document.createElement('ul');
+
+  if (!data || Object.keys(data).length === 0) {
+    return;
+  }
+
+  if (element === null) {
+    ulElem.setAttribute('id', 'tree');
+    document.body.append(ulElem);
+  } else {
+    element.appendChild(ulElem);
+  }
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.innerHTML = key;
+    ulElem.appendChild(li);
+
+    createTree(li, data[key]);
+  }
 }
 
 createTree(tree, food);
